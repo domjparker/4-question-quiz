92f8bfd6ba4ec3e5947cb281efccf4680551199c
Rees Dart <thedominicparker@gmail.com>
2020-07-01 09:05:04 -0700

    changes scoring so that incorrect answers subtract seconds from timer.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var startPage = document.querySelector("#startPage");
 var startBtnDiv = document.querySelector("#startBtnDiv");
 var hiScoreBtn = document.querySelector("#highScoreButton")
 var timerDisplayID = document.querySelector("#timer");
+var pageBody = document.querySelector("#pageBody")
 
 // variables made from elements grabbed from the quiz pages
 var quizPage = document.querySelector("#quiz");
@@ -29,7 +30,7 @@ var deleteScoresBtn = document.querySelector("#deleteScores");
 var rankItem = "";
 
 // seconds amount for the timer
-var secondsLeft = 20
+var secondsLeft = 40
 
 // score tally of how many answers are chosen correctly 
 var scoreCount = 0
@@ -70,33 +71,43 @@ var questionBank = [
 function init() {
     // displayStartPage()
     displayStartPage();
-    // button to start quiz
+    // button created, given text, and appended
     var startBtn = document.createElement("button")
     startBtnDiv.className = "btn btn-success";
     startBtn.textContent = "Start Game";
     startBtnDiv.appendChild(startBtn);
     // when start button clicked
     startBtn.addEventListener("click", function() {
-        scoreCount = 1;
+        scoreCount = 0;
         displayQuizPage();
         populateQuesAns();
         startTimer();
         
-    })
-}
+    })}
+
+// if High Scores button clicked, high scores page is displayed.
+hiScoreBtn.addEventListener("click", function() {
+    displayHighScoresPage()
+})
 
 // function to push question and answer details to the DOM as they cycle through the questionBank array.
 function populateQuesAns() {
+    // choices cleared from any previous question cycles
     choicesID.innerHTML = "";
+    // question for current index count given text and populated to quiz page
     questionID.textContent = questionBank[indexCount].question;
+    // for each of the possible answers ...
     questionBank[indexCount].possibleAnswers.forEach(function (option) {
+        // ...button element is created
         var button = document.createElement("button")
+        // ...possible answer is given to the button
         button.textContent = option
+        // ... and the button with text content is appended to the quiz page
         choicesID.appendChild(button)
     })
     
 }
-// click event listener for each button populated for each question.
+// click event listener is given to each button populated for each question.
 choicesID.addEventListener("click",function (event) {
     choiceResult.innerHTML = "";
     // If the click event is a button clicked...
@@ -105,16 +116,20 @@ choicesID.addEventListener("click",function (event) {
         if(indexCount < questionBank.length - 1) {
             // and if the button clicked corresponds to the correct answer for that question...
             if(event.target.textContent === questionBank[indexCount].correctAnswer) {
-                // give a congrats message.
-                var congrats = document.createElement("a");
+                // a congrats element is created and ...
+                var congrats = document.createElement("h1");
+                // ... is given classes,
                 congrats.className = "card-body text-success"
+                // ... is given text content,
                 congrats.textContent = "Correct!!! You know your stuff!!!"
+                // and is appended to the page for the duration of the next question
                 choiceResult.appendChild(congrats);
-                // add to the index count in order to move to the next question
-                indexCount++;
-                // add one point to the user's score.
+                // index count increases by 1 in order to move to the next question
+                // indexCount++;
+                // one point is added to the user's score.
                 scoreCount++
-                // and populate the next question
+                console.log(scoreCount)
+                // and quiz moves on to the next question
                 populateQuesAns()
             } 
             else {      //if the button clicked corresponds with a wrong answer, and is not the final question....
@@ -124,7 +139,9 @@ choicesID.addEventListener("click",function (event) {
                 wrongAnswer.textContent = "You need to study more!"
                 choiceResult.appendChild(wrongAnswer);
                 // move to the next question
-                indexCount++;
+                // indexCount++;
+                // deduct time from timer
+                secondsLeft= secondsLeft -10
                 // subtract a point from the user's score.
                 scoreCount--;
                 //populate next question
@@ -137,6 +154,9 @@ choicesID.addEventListener("click",function (event) {
     }
 })
 
+
+
+
 // timer function to count down from 20 seconds
 function startTimer() {
     var timerInterval = setInterval(function() {
@@ -144,12 +164,13 @@ function startTimer() {
         //populate timer display
         timerDisplayID.textContent = "time: " + secondsLeft;
         // if timer gets to zero, move to user result page.
-        if(secondsLeft === 0) {
+        if(secondsLeft === 0 ) {
             clearInterval(timerInterval);
             // alert("You ran out of time");
             displayResultPage()
-        // if user answers questions within 20 seconds, log time when finished fourth question.
-        } else if (indexCount === questionBank.length - 1) {  //having an issue where timer stops after 3 clicks or not at all if I take away the '- 1'
+        
+        // if user answers questions within 0 seconds, log time when finished fourth question.
+        } else if (indexCount === questionBank.length -1) {  //having an issue where timer stops after 3 clicks or not at all if I take away the '- 1'
             clearInterval(timerInterval);
             console.log(timerDisplayID.textContent);
         }
@@ -160,6 +181,8 @@ function startTimer() {
 
 // populate users result to result page.
 function populateResult() {
+    
+    
     // If user got no answer's correct, say this
     if (scoreCount <= 0) {
         yourResult.textContent = "You chose no correct answers.";
@@ -171,29 +194,44 @@ function populateResult() {
 // button to submit initials to high scores page
 submitBtn.addEventListener("click", function() {
     event.preventDefault();
+    if (initials.value !==""){
+
+    
     userDetails = {
         name: initials.value,
-        score: scoreCount,
-        time: timerDisplayID.textcontent
+        score: secondsLeft,
+        time: secondsLeft
     } //having an issue where name, score, and time are not defined in line 170
+
+// Write the logic to order the list
+
+    if(userDetailsArray.length >10){
+        userDetailsArray.shift()
+    }
     userDetailsArray.push(userDetails);
     initials.value = "";
     localStorage.setItem("userDetailsArray", JSON.stringify(userDetailsArray));
-    for (let i = 0; i < 10; i++) {
-        rankItem = userDetailsArray[i];
+}
+if(userDetailsArray.length >0){
+    console.log(userDetailsArray);
+    
+    for (let i = 0; i < userDetailsArray.length; i++) {
+        //rankItem = userDetailsArray[i];
         var rankItemLI = document.createElement("div");
         rankItemLI.className = "bg-primary border";
-        rankItemLI.textContent = "Name: " + rankItem.name + "______Score: " + rankItem.score + "______Time Left: " + rankItem.time;
+        rankItemLI.textContent = "Name: " + userDetailsArray[i].name + "______Score: " + userDetailsArray[i].score + "______Time Left: " + userDetailsArray[i].time;
         scoreListDiv.appendChild(rankItemLI); 
     }
+
     displayHighScoresPage();
+}
 })
 
 
 // If Play Again! button clicked, user taken back to start page and can play again.
 playAgainBtn.addEventListener("click", function() {
     displayStartPage();
-    secondsLeft = 20;
+    secondsLeft = 40;
     indexCount = 0;
     scoreCount = 0;
 })
@@ -201,9 +239,8 @@ playAgainBtn.addEventListener("click", function() {
 // If Delete Scores button clicked, scores are deleted.
 deleteScoresBtn.addEventListener("click", function() {
     localStorage.clear();
-    rankItemLI.textContent = ""
+    scoreListDiv.innerHTML=""
 })
-
 
 // Display Page Functions
 function displayStartPage(){
@@ -213,6 +250,7 @@ function displayStartPage(){
     highScorePage.style.display = "none";
     timerDisplayID.style.display = "block";
     hiScoreBtn.style.display = "block";
+    pageBody.style.background = "darkorange";
 }
 
 function displayQuizPage(){
@@ -222,6 +260,7 @@ function displayQuizPage(){
     highScorePage.style.display = "none";
     timerDisplayID.style.display = "block";
     hiScoreBtn.style.display = "block";
+    pageBody.style.background = "cyan";
 }
 
 function displayResultPage(){
@@ -231,6 +270,7 @@ function displayResultPage(){
     highScorePage.style.display = "none";
     hiScoreBtn.style.display = "none";
     timerDisplayID.style.display = "block";
+    pageBody.style.background = "hotpink";
 }
 
 function displayHighScoresPage(){
@@ -240,6 +280,7 @@ function displayHighScoresPage(){
     highScorePage.style.display = "block";
     hiScoreBtn.style.display = "none";
     timerDisplayID.style.display = "none";
+    pageBody.style.background = "blueviolet";
 }
 
 init()
